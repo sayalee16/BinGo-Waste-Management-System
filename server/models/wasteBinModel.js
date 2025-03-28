20f4dee698c1273d327951089eb2697b8570d2be
swakhare23 <swapnapkhare@gmail.com>
2025-03-28 21:16:00 +0530

    Added wasteBin schema

---
+++ server/models/wasteBinModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const wasteBinSchema = new Schema({
+    totalCapacity: {
+        type: Number,
+        required: true
+    },
+
+
+    realTimeCapacity: {
+        type: Number,
+        required: true
+    },
+    approxTimeToFill: {
+        type: Number,  
+        required: true
+    },
+    lastEmptiedAt: {
+        type: Date,
+        required: true
+    },
+    
+    location: {
+        type: {
+            type: String,   
+            enum: ["Point"],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    binType: {
+        type: String,
+        enum: ["public", "residential", "industrial"],
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ["plastic", "wet", "dry"],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["filled", "recycled", "partially filled"],
+        required: true
+    },
+    sensorEnabled: {
+        type: Boolean,
+        required: true
+    },  
+    ward: {
+        type: String,
+        required: true
+    },
+    zone: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.model("WasteBin", wasteBinSchema);
