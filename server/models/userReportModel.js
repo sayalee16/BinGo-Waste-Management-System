742006c484ee7a397edb6845be51e9c6134379fa
swakhare23 <swapnapkhare@gmail.com>
2025-03-28 22:38:25 +0530

    Added userReport schema

---
+++ server/models/userReportModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userReportSchema = new Schema({
+    bin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "WasteBin",
+        required: true
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",  
+        required: true
+    },
+
+
+
+
+    
+    status: {
+        type: String,
+        enum: ["full", "damaged", "needs maintenance", "partially filled"],
+        required: true
+    },
+    attachment: {
+        type: String,  
+        required: true
+    },
+
+
+
+    description: {
+        type: String,  
+        required: false  
+    }
+});  
+
+export default mongoose.model("UserReport", userReportSchema);
