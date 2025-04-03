913bb5b19d6dfb21a3829ab4170f044c76e40f4c
swakhare23 <swapnapkhare@gmail.com>
2025-04-03 14:09:59 +0530

    updated user-report

--- server/routes/userReportRoute.js
+++ server/routes/userReportRoute.js
@@ -18,9 +18,27 @@ router.get("/get-report/:id", authenticateUser,getReportById);
 //create report
 router.post("/create-report", authenticateUser,createReport); 
 
-
+//check if admin
+const authorizeAdmin = (req, res, next) => {
+  if (!req.isAdmin ) {
+      return res.status(403).json({ msg: "Access denied. Admins only." });
+  }
+  next();
+};
 //update report
-router.put("/update-report/:id", authenticateUser, updateReport); 
+router.put("/update-report/:id", authenticateUser,authorizeAdmin, updateReport); 
+
+//check if waste collector
+const authorizeWasteCollector = (req, res, next) => {
+  if (!req.isWasteCollector) {
+
+    return res.status(403).json({ msg: "Access denied. WasteCollectors only." });
+  }
+  next();
+};
+//update report
+router.put("/wc-update-report/:id", authenticateUser,authorizeWasteCollector, updateReport); 
+
 
 //delete report
 router.delete("/delete-report/:id", authenticateUser, deleteReport); 
